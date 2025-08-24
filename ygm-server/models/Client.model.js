70fe80a1da51884cdd3488d23a0d8989a434b90d
DheerajRay-01 <dheerajray2003@gmail.com>
2025-08-24 14:21:26 +0530

    update

--- ygm-server/models/Client.model.js
+++ ygm-server/models/Client.model.js
@@ -1,29 +1,3 @@
-// import mongoose from 'mongoose';
-
-// const clientSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: true,
-//   },
-//   description: String,
-//   logo: String,
-//   website: {
-//     type: String,
-//     default: "#",
-//   },
-//   serviceType: {
-//     type: String,
-//   },
-//   duration: String,
-//   serviceID: {
-//     type: mongoose.Schema.Types.ObjectId, // MongoDB default _id from Service
-//     ref: "Service",                       // Reference to Service collection
-//   }
-// }, { timestamps: true });
-
-// export default mongoose.model("Client", clientSchema);
-
-
 import mongoose from 'mongoose';
 
 const clientSchema = new mongoose.Schema({
@@ -44,13 +18,7 @@ const clientSchema = new mongoose.Schema({
   serviceID: {
     type: mongoose.Schema.Types.ObjectId, // MongoDB default _id from Service
     ref: "Service",                       // Reference to Service collection
-  },
-  // order: {
-  //   type: Number,
-  //   default: 0, // By default sabka 0 hoga
-  // }
+  }
 }, { timestamps: true });
 
-const Client = mongoose.model("Client", clientSchema);
-
-export default Client;
+export default mongoose.model("Client", clientSchema);
