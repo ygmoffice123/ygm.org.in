1844c24f10ef18d82aa4e0fd22b97ab8ff6a0252
DheerajRay-01 <dheerajray2003@gmail.com>
2025-09-05 23:17:39 +0530

    change video

--- ygm/src/data/imageAndVideos.js
+++ ygm/src/data/imageAndVideos.js
@@ -1,6 +1,7 @@
-export const heroBgImage = "https://images.pexels.com/photos/1181438/pexels-photo-1181438.jpeg"
+export const heroBgImage = "https://res.cloudinary.com/drfh0aszz/image/upload/v1756982828/emp_a9khio.png"
+// export const heroBgImage = "https://res.cloudinary.com/drfh0aszz/image/upload/v1757070173/emp3_jkreus.png"
 
-export const slide1_img = "https://images.pexels.com/photos/269077/pexels-photo-269077.jpeg"
+export const slide1_img = "https://res.cloudinary.com/drfh0aszz/image/upload/v1757070502/emp4_mf6jcz.png"
 export const slide1_vid = "https://res.cloudinary.com/drfh0aszz/video/upload/v1756877272/ygm_bg1_awozyo.mp4"
 
 export const slide2_img = "https://images.unsplash.com/photo-1497366811353-6870744d04b2?auto=format&fit=crop&w=1600&q=80"
