f5a6deea1e45c4f98cdb4a9bd665a8b54ad5b21b
DheerajRay-01 <dheerajray2003@gmail.com>
2025-09-23 17:09:59 +0530

    fix line chart

--- ygm/src/data/lineChartData.js
+++ ygm/src/data/lineChartData.js
@@ -1,8 +1,9 @@
 export   const employeesData = [
-    { year: 2019, employees: 40 },
-    { year: 2020, employees: 60 },
-    { year: 2021, employees: 80 },
-    { year: 2022, employees: 120 },
-    { year: 2023, employees: 150 },
-    { year: 2024, employees: 200 },
+    { year: 2019, employees: 150 },
+    { year: 2020, employees: 200 },
+    { year: 2021, employees: 300 },
+    { year: 2022, employees: 450 },
+    { year: 2023, employees: 550 },
+    { year: 2024, employees: 700 },
+    { year: 2025, employees: 800 },
   ];
